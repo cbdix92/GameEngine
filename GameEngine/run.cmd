6ce4e1561109d5e06e5353b2190e173d53e1c251
cbdix92 <cbdix92@gmail.com>
2018-07-12 22:14:43 -0400

    Compiler script added
    Compiler script added

--- GameEngine/run.cmd
+++ GameEngine/run.cmd
@@ -2,13 +2,11 @@
 
 :: Delete Exeecutable and the libs so compile errors aren't ignored due to an older version being ran. 
 del test.exe
-del Core.dll
-del GameEngine.dll
+del CompileLibs\GameEngine.dll
 
 :: Compile Libs
-:: C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:library /out:Core.dll Core.cs
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:library /out:GameEngine.dll /r:PresentationCore.dll /r:WindowsBase.dll Animation.cs Background.cs Core.cs Display.cs Events.Cs GameLoop.cs Gameobject.cs Image.cs Input.cs ObjectStateMachine.cs Render.cs Scene.cs Transform.cs Updates.cs 
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:library /out:CompileLibs\GameEngine.dll /r:CompileLibs\PresentationCore.dll /r:CompileLibs\WindowsBase.dll Animation.cs Background.cs Core.cs Display.cs Events.Cs GameLoop.cs Gameobject.cs Image.cs Input.cs ObjectStateMachine.cs Render.cs Scene.cs Transform.cs Updates.cs 
 
 :: Compile Main
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /r:GameEngine.dll test.cs
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /r:CompileLibs\GameEngine.dll test.cs
 test.exe
